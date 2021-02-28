84763fe2de17e2277c60a18875246c6c824c5e1a
Wesley Ting <wesleyting3@gmail.com>
2021-02-27 21:15:30 -0800

    Added scrollReveal.js function to previews

---
+++ scripts/scrollReveal.js
@@ -0,0 +1,11 @@
+// Code found at: https://scrollrevealjs.org/
+
+
+ScrollReveal({ distance: '60px' });
+// ScrollReveal().reveal('.work-1', { duration: 1000 }, { origin: 'top' });
+// ScrollReveal().reveal('.work-2', { duration: 1000 }, { origin: 'top' });
+// ScrollReveal().reveal('.work-3', { duration: 1000 }, { origin: 'top' });
+// ScrollReveal().reveal('.work-4', { duration: 1000 }, { origin: 'top' });
+
+ScrollReveal().reveal('.work', { inverval: 300 }, { origin: top }, { delay: 300 });
+
