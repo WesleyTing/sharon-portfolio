8f3035305c557e6be8c67e6a2ede4e2e9a8e5f98
Wesley Ting <wesleyting3@gmail.com>
2021-02-28 21:53:37 -0800

    Edited scrollReveal.js with correct classes

--- scripts/scrollReveal.js
+++ scripts/scrollReveal.js
@@ -7,5 +7,5 @@ ScrollReveal({ distance: '60px' });
 // ScrollReveal().reveal('.work-3', { duration: 1000 }, { origin: 'top' });
 // ScrollReveal().reveal('.work-4', { duration: 1000 }, { origin: 'top' });
 
-ScrollReveal().reveal('.work', { inverval: 300 }, { origin: top }, { delay: 300 });
+ScrollReveal().reveal('.grid-item', { inverval: 300 }, { origin: top }, { delay: 300 });
 
