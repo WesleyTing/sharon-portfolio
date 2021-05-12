66c736fb98444a5732c6192714f86746f80cfd4a
Wesley Ting <wesleyting3@gmail.com>
2021-05-11 20:53:17 -0700

    Create mySlickScript.js

---
+++ scripts/mySlickScript.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.slider-sketch').slick({
+      slidesToShow: 1,
+    slidesToScroll: 1,
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 5000,
+
+    });
+  });
